99051c17bad9f4725f91c25e12892f768d4b7848
Amine1Amine <334491@student.mboutrecht.nl>
2023-10-23 20:58:13 +0200

    first commit

---
+++ mvc-2209a-P4_2/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'Mvc-Oop-2209a-P2');
+define('URLROOT', 'http://www.rijschool.nl/');
+define('APPROOT', dirname(dirname(__FILE__)));
---
+++ mvc-2209a-P4_2/app/views/Homepage/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Inlogregistratie</title>
+</head>
+<body>
+    <h1>Amine's MVC-opdracht2</h1>
+    <h3><?= $data['title']; ?></h3>
+    <a href="<?= URLROOT; ?>/Instructeur/overzichtinstructeur">Instructeurs in dienst</a> |
+</body>
+</html>
